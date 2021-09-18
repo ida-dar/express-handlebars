9958ee75ea81550a616bb0a4d1096c4609d5a400
Ida Darmochwał <ida.mdart@gmail.com>
2021-09-18 16:29:08 +0200

    Add Handlebars

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs()); // defines that certain files should be rendered by given engine. '.hbs' (dot is optional, Express will add it by itself) files should be handled by the 'hbs engine' (loaded Handlebars).
+app.set('view engine', '.hbs'); // in the app we use files with '.hbs' extention, so Express will know that it should look for a file with the appropriate suffix.
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' }, { layout: false });
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', { layout: false });
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', { layout: false });
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', { layout: false });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name }); // the engine will look for 'hello' view in folder 'views' by default. Handlebars uses 'layouts', to render one simple view it we will turn it off.
 });
 
 app.use((req, res) => {
